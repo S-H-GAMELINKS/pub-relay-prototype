ed55dfa72afa8d594e6c0d0a0dfab949731afc1b
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-12-10 21:32:57 +0900

    Remove old controller spec's

--- spec/controllers/inboxes_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe InboxesController, :type => :controller do
-  describe 'POST #create' do
-    pending
-  end
-end
--- spec/controllers/webfinger_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe WebfingerController, :type => :controller do
-  describe 'GET #show' do
-    pending
-  end
-end
