1ff54881574dde0d351f079b40689dd08c07870b
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-12-14 01:09:24 +0900

    Bump up to Rails 7.2

--- app/controllers/webfinger_controller.rb
+++ app/controllers/webfinger_controller.rb
@@ -18,6 +18,6 @@ class WebfingerController < ApplicationController
   private
 
   def acct
-    'acct:relay@' + Rails.application.default_url_options[:host]
+    "acct:relay@#{ENV["DOMAIN"]}"
   end
 end
